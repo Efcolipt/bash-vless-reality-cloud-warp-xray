23a31a0162f1da9dbecb9115a988d88bfbe97177
Baykov Ivan <45362427+Efcolipt@users.noreply.github.com>
2026-02-02 19:00:47 +0300

    chore: change domain

--- vless-reality-cloud-warp.sh
+++ vless-reality-cloud-warp.sh
@@ -6,7 +6,7 @@ WARN="[WARN]"
 ERROR="[ERROR]"
 
 XRAY_PATH_CONFIG="/usr/local/etc/xray/config.json"
-MASK_DOMAIN="cdnjs.cloudflare.com"
+MASK_DOMAIN="www.cloudflare.com"
 
 export XRAY_PATH_CONFIG
 
