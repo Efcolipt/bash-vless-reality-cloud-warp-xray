33ccd350d55b61b9297dd8f201153df85030a297
Baykov Ivan <45362427+Efcolipt@users.noreply.github.com>
2026-02-03 11:18:01 +0300

    chore: update

--- vless-reality-cloud-warp-with-3x-ui.sh
+++ vless-reality-cloud-warp-with-3x-ui.sh
@@ -426,14 +426,14 @@ JSON
   config_webBasePath=$(gen_random_string 18)
 
 
-  "${xui_folder}/x-ui" setting -username "${config_account}" -password "${config_password}" -resetTwoFactor false >/dev/null 2>&1
-  "${xui_folder}/x-ui" setting -webBasePath "${config_webBasePath}" >/dev/null 2>&1
+  "$xui_folder/x-ui" setting -username "$config_account" -password "$config_password" -resetTwoFactor false >/dev/null 2>&1
+  "$xui_folder/x-ui" setting -webBasePath "$config_webBasePath" >/dev/null 2>&1
 
   echo -e "Panel login username: ${config_account}"
   echo -e "Panel login password: ${config_password}"
   echo -e "Web base path: ${config_webBasePath}"
 
-  ${xui_folder}/x-ui settings -y
+  "$xui_folder/x-ui" settings -y
 
   # read -r -p "PORT: " XUI_PORT
   # read -r -p "USER: " XUI_USER
