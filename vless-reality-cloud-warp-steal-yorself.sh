6c9586f6c9f027ba5e656382e576ee18ee976b96
Baykov Ivan <45362427+Efcolipt@users.noreply.github.com>
2026-02-09 00:43:56 +0300

    chore: update

--- vless-reality-cloud-warp-steal-yorself.sh
+++ vless-reality-cloud-warp-steal-yorself.sh
@@ -121,7 +121,7 @@ install_vps() {
 install_deps() {
   log "Installing deps"
   apt update
-  apt install dig dnsutils iptables fail2ban netfilter-persistent iptables-persistent curl jq openssl -y
+  apt install dnsutils iptables fail2ban netfilter-persistent iptables-persistent curl jq openssl -y
 
 
   if ! command -v docker 2>&1 >/dev/null; then
