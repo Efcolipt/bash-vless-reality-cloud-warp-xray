d6afc254958d704f3b71dbc4b87179a2f1465fef
Baykov Ivan <45362427+Efcolipt@users.noreply.github.com>
2026-05-18 00:45:07 +0300

    chore(hy2): perf nets for config ration 2:5

--- hysteria2-cloud-warp-masquerade-yourself.sh
+++ hysteria2-cloud-warp-masquerade-yourself.sh
@@ -235,24 +235,26 @@ set_nets() {
   cat >/etc/sysctl.d/99-bbr-tune.conf <<'EOF'
 net.core.rmem_max=67108864
 net.core.wmem_max=67108864
-net.core.netdev_max_backlog=10000
-net.core.somaxconn=4096
-net.ipv4.tcp_syncookies=1
-net.ipv4.tcp_tw_reuse=1
-net.ipv4.tcp_fin_timeout=30
-net.ipv4.tcp_keepalive_time=1200
-net.ipv4.tcp_keepalive_probes=5
-net.ipv4.tcp_keepalive_intvl=30
-net.ipv4.tcp_max_syn_backlog=8192
-net.ipv4.tcp_max_tw_buckets=5000
-net.ipv4.tcp_fastopen=3
+net.core.rmem_default=4194304
+net.core.wmem_default=4194304
+
+net.core.netdev_max_backlog=5000
+net.core.somaxconn=2048
+
+net.ipv4.udp_mem=16384 32768 65536
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
+
 net.ipv4.tcp_rmem=4096 87380 67108864
 net.ipv4.tcp_wmem=4096 65536 67108864
-net.ipv4.udp_mem=4096 51200 102400
-net.ipv4.tcp_mtu_probing=1
-net.ipv4.tcp_slow_start_after_idle=0
+
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
+
+net.ipv4.tcp_mtu_probing=1
+net.ipv4.tcp_slow_start_after_idle=0
+net.ipv4.tcp_syncookies=1
+net.ipv4.tcp_max_syn_backlog=4096
 EOF
 
   sysctl --system >/dev/null
